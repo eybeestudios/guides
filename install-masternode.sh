205f41281cbb644e6232d1222c72f776a9d1451f
Arne <no>
2018-05-31 23:48:20 +0200

    first beta version

--- install-masternode.sh
+++ install-masternode.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+YELLOW='\e[1;33m'
+NC='\033[0m' # No Color
+
+LOCAL_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 
 function readJsonValue {
-  UNAMESTR=`uname`
-  if [[ "$UNAMESTR" == 'Linux' ]]; then
-    SED_EXTENDED='-r'
-  elif [[ "$UNAMESTR" == 'Darwin' ]]; then
-    SED_EXTENDED='-E'
-  fi;
-
   LINE=`grep -m 1 "\"${2}\"" ${1} | cut -d ":" -f 2`
 
   if [ ! "$LINE" ]; then
@@ -16,13 +16,16 @@ function readJsonValue {
     #exit 1;
   else
     VALUE=${LINE//,}
-    echo "${VALUE##*( )}" ;
+    echo $VALUE | sed 's/^[ \t]*//;s/[ \t]*$//'
   fi;
 }
 
 function getBlockchainHeightFromNode {
   ./ips-cli getinfo >> node.info
   block=`readJsonValue node.info blocks`
+  if [ "$block" = "NOT-FOUND" ]; then
+    block=0
+  fi
   rm node.info
   echo $block ;
 }
@@ -56,10 +59,8 @@ function waitForMasternodePayment {
   do
     ./ips-cli listtransactions 0 1 0 > transaction.last
     amount=`readJsonValue transaction.last amount`
-    echo "--$amount--"
     if [ "$amount" = "5000.00000000" ]
     then
-      echo "amount: $amount"
       txid=`readJsonValue transaction.last txid`
       echo ${txid//\"}
       rm transaction.last
@@ -72,15 +73,30 @@ function waitForMasternodePayment {
 function createMasternodeConfig {
   echo
   echo "CREATING MASTERNODE CONFIGURATION"
+
+  MASTERNODE_ALIAS="$1"
+
+  TX_HASH="NOT-FOUND"
+  OUTPUT_IDX="NOT-FOUND"
+
+  while [ "$TX_HASH" = "NOT-FOUND" ]
+  do
+    sleep 5
+    ./ips-cli masternode outputs > masternode_outputs
+    TX_HASH=`readJsonValue masternode_outputs txhash`
+    TX_HASH=$( echo $TX_HASH | sed 's/^"//;s/"$//')
+    OUTPUT_IDX=`readJsonValue masternode_outputs outputidx`
+  done
+
+  MASTERNODE_PRIVATE_KEY=$(<masternode_key)
+
+  # alias IP:port masternodeprivkey collateral_output_txid collateral_output_index
+  echo "$MASTERNODE_ALIAS $LOCAL_IP:22331 $MASTERNODE_PRIVATE_KEY $TX_HASH $OUTPUT_IDX" >> ~/.ips/masternode.conf
+
+  rm masternode_outputs
 }
 
 function install {
-
-  #!/bin/bash
-  RED='\033[0;31m'
-  GREEN='\033[0;32m'
-  CYAN='\033[0;36m'
-  NC='\033[0m' # No Color
 
   echo "--------------------------------------------------------------------------------"
   echo "--                                                                            --"
@@ -93,7 +109,14 @@ function install {
   echo
 
   read -p "DO YOU WANT TO INSTALL A NEW IPSUM MASTERNODE? [yes/no]: " INSTALL
+  echo
   read -p "TYPE THE ALIAS FOR YOUR MASTERNODE: " MASTERNODE_ALIAS
+
+  if [ -z "$MASTERNODE_ALIAS" ]
+  then
+    echo "No ALIAS supplied, alias will be: IPS-MN"
+    MASTERNODE_ALIAS="IPS-MN"
+  fi
 
   if [ "$INSTALL" != "yes" ]; then
     exit;
@@ -101,19 +124,8 @@ function install {
 
   echo
 
-  CONF_DIR=~/.ips/
-  if [ -d CONF_DIR ]; then
-    rm ips*
-    rm -r ~/.ips
-  fi
-  mkdir -p $CONF_DIR
-
   sudo apt-get -y update
   sudo apt-get -y upgrade
-  wget https://github.com/ipsum-network/ips/releases/download/v3.1.0.0/ips-3.1.0-linux.tar.gz
-  tar xvzf ips-3.1.0-linux.tar.gz
-  cp ./ips-3.1.0/bin/ips-cli .
-  cp ./ips-3.1.0/bin/ipsd .
   sudo apt autoremove -y && sudo apt-get update
   sudo apt-get install -y libzmq3-dev libminiupnpc-dev libssl-dev libevent-dev
   sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config
@@ -122,9 +134,18 @@ function install {
   sudo add-apt-repository ppa:bitcoin/bitcoin -y
   sudo apt-get update
   sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
-  mkdir -p ~/.ips
 
-  LOCAL_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+  CONF_DIR=~/.ips/
+  if [ -d CONF_DIR ]; then
+    rm ips*
+    rm -r ~/.ips
+  fi
+  mkdir -p $CONF_DIR
+
+  wget https://github.com/ipsum-network/ips/releases/download/v3.1.0.0/ips-3.1.0-linux.tar.gz
+  tar xvzf ips-3.1.0-linux.tar.gz
+  cp ./ips-3.1.0/bin/ips-cli .
+  cp ./ips-3.1.0/bin/ipsd .
 
   CONF_FILE=ips.conf
   CONF_TEMP=temp.conf
@@ -136,7 +157,7 @@ function install {
   echo "listen=1" >> CONF_TEMP
   echo "server=1" >> CONF_TEMP
   echo "daemon=1" >> CONF_TEMP
-  echo "staking=1" >> CONF_TEMP
+  echo "staking=0" >> CONF_TEMP
   echo "txindex=1" >> CONF_TEMP
   echo "logtimestamps=1" >> CONF_TEMP
   echo "port=22331" >> CONF_TEMP
@@ -146,7 +167,6 @@ function install {
   LINES=$(< README.md wc -l)
   END=$((LINES - 1))
   sed -n "7,$END p" < README.md > SEED_NODES
-  rm README.md
 
   cat CONF_TEMP SEED_NODES > $CONF_DIR/$CONF_FILE
 
@@ -155,13 +175,59 @@ function install {
   checkBlockchainHeightAndWaitForSync
 
   masternode_key=$(./ips-cli masternode genkey)
-  echo $masternode_key >> masternode_key
+  echo $masternode_key > masternode_key
   masternode_wallet=$(./ips-cli getaccountaddress 0)
-  echo $masternode_wallet >> masternode_wallet
+  echo $masternode_wallet > masternode_wallet
 
-  echo "NOW SEND EXACTLY 5000 IPS TO THIS ADDRESS: $masternode_wallet"
+  echo -e "NOW SEND ${RED}EXACTLY ${YELLOW}5000${NC} IPS TO THIS ADDRESS: ${CYAN}$masternode_wallet${NC}"
 
   waitForMasternodePayment
+
+  createMasternodeConfig $MASTERNODE_ALIAS
+
+  ./ips-cli stop
+
+  echo "masternode=1" >> CONF_TEMP
+  echo "masternodeprivkey=$masternode_key" >> CONF_TEMP
+  echo "" >> CONF_TEMP
+
+  cat CONF_TEMP SEED_NODES > $CONF_DIR/$CONF_FILE
+
+  # start masternode
+  ./ipsd
+
+  sleep 5
+
+  ./ips-cli startmasternode alias 0 $MASTERNODE_ALIAS
+
+  sleep 1
+
+  ./ips-cli masternode start $MASTERNODE_ALIAS
+
+  sleep 1
+
+  ./ips-cli masternode status > masternode_status
+  MESSAGE=`readJsonValue masternode_status message`
+  if ! [[ $MESSAGE = *"successfully"* ]]; then
+    echo
+    echo -e "${RED}!!! - COULD NOT START MASTERNODE - !!!${NC}"
+    exit
+  fi
+
+  # enable staking
+  ./ips-cli stop
+  sed -i '/staking=0/c\staking=1' $CONF_DIR/$CONF_FILE
+
+  ./ipsd
+
+  # remove all temp files
+  rm masternode_status
+  rm README.md
+  rm SEED_NODES
+  rm CONF_TEMP
+
+  # AND DONE!
+  echo -e "${GREEN}Congratulations!!! Your masternode $MASTERNODE_ALIAS is up and running!${NC}"
 }
 
 case "$1" in
@@ -173,9 +239,6 @@ case "$1" in
        ;;
     status)
        checkBlockchainHeightAndWaitForSync
-       ;;
-    payment)
-      waitForMasternodePayment
        ;;
     *)
        echo "Usage: $0 {install|update|status}"
